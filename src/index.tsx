d43ad1e378644aa69eef095ff197f40f24b5ef02
rzalyaev <ruslan.zalyaev@gmail.com>
2023-06-24 21:32:08 +0400

    Add Provider and change AppWithReducers to AppWithRedux

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './index.css';
 import { createRoot } from 'react-dom/client';
 import * as serviceWorker from './serviceWorker';
-import AppWithReducers from "./AppWithReducers";
+import { store } from './state/store'
+import { Provider } from 'react-redux'
+import AppWithRedux from "./AppWithRedux";
 
 const container  = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
-root.render(<AppWithReducers />);
+root.render(
+    <Provider store={store}>
+        <AppWithRedux/>
+    </Provider>
+);
 
 
 // If you want your app to work offline and load faster, you can change
